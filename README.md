d383cc54c18f19b7d7d94cd04c5e7b54daee5617
Lukas Matt <lukas@matt.wf>
2018-12-12 16:36:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Dummy Project
+-------------
+
+Related to [feneas.git.feneas.org/federation-testsuite](https://feneas.git.feneas.org/federation-testsuite/#testsuite)
---
+++ testproject.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+[ "$#" -ne 3 ] && {
+  echo "$0 [DATABASE] [DATABASEHOST] [PORT]"
+  exit 1
+}
+
+DATABASE=$1
+DATABASEHOST=$2
+PORT=$3
+
+psql="psql -h $DATABASEHOST -U postgres -d $DATABASE"
+psqlAdd="$psql -c 'insert into testtable values(1);'"
+psqlRemove="$psql -c 'delete from testtable where id = 1;'"
+psqlCount="$psql -q -c 'select count(*) from testtable;'"
+
+# create db schema
+$psql -c 'create table if not exists testtable(id serial primary key);' || exit 1
+
+while true; do
+  cnt=$(eval $psqlCount |head -3 |tail -n 1)
+  req=$(echo -e "HTTP/1.1 200 OK\n\n$cnt" | nc -c -l -p $PORT 0.0.0.0)
+  if [[ $req =~ '/add' ]]; then
+    eval $psqlAdd
+  fi
+  if [[ $req =~ '/remove' ]]; then
+    eval $psqlRemove
+  fi
+done
