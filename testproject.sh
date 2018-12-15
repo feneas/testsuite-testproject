80790127d703a963d99a918bf32f76b6a30ef24d
Lukas Matt <lukas@matt.wf>
2018-12-15 15:08:38 +0100

    Move to nmap netcat and accept all connections

--- testproject.sh
+++ testproject.sh
@@ -17,10 +17,14 @@ psqlCount="$psql -q -c 'select count(*) from testtable;'"
 # create db schema
 $psql -c 'create table if not exists testtable(id serial primary key);' || exit 1
 
-while true; do
-  cnt=$(eval $psqlCount |head -3 |tail -n 1 |tr -d '[:space:]')
-  req=$(echo -e "HTTP/1.1 200 OK\n\n$cnt" |nc -l -p $PORT -q1)
-  remote=$(echo "$req" |grep GET |cut -d' ' -f2 |cut -d@ -f2)
+tmplog=$(mktemp)
+# listen and serve
+ncat -l $PORT -o $tmplog -k -c 'echo -e "HTTP/1.1 200 OK\n\n$('\
+'eval $psqlCount |head -3 |tail -n 1 |tr -d \"[:space:]\")"' &
+
+while inotifywait -qq -e close_write $tmplog; do
+  req=$(grep GET $tmplog |tail -1)
+  remote=$(echo $req |cut -d' ' -f2 |cut -d@ -f2)
   if [[ $req =~ '/add' ]]; then
     if [[ "$remote" == "/add" ]]; then
       eval $psqlAdd
